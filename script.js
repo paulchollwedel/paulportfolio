9aafd432573e4a7e7e805a0cc8e1ab1a0a2b2ece
Paul Hollwedel <paulchollwedel@gmail.com>
2019-11-18 22:42:47 -0800

    updated portfolio page

--- script.js
+++ script.js
@@ -2,17 +2,17 @@ $("#aboutMe").click(function() {
     
     $("#paulPhoto").empty().append(
         '<div class="rounded" id="picBackground"> <img class="rounded" id = "photo" src="IMG_0055 (1).jpg" height="50%" width="40%"> <h1>About Me</h1> <p class="paragraph">Lorem ipsum dolor sit amet, dolor quam scelerisque libero, vel sollicitudin lorem lorem elementum arcu. Integer eget dui risus. In rutrum nisl in semper convallis. Donec justo nulla, molestie at pulvinar in, consequat eu lacus. Cras faucibus, justo non imperdiet maximus, justo ligula auctor nisi, ut imperdiet ex ex ac orci. Nam ultrices dolor enim, sit amet elementum lorem ornare nec. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In eu viverra lectus, ac vestibulum turpis. Etiam sit amet turpis ex. Vestibulum imperdiet suscipit augue, id sagittis libero vehicula a. Donec sed massa magna. s elit ut, accumsan congue lacus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque sit amet est sed nisl mollis maximus id nec ligula. Nulla posuere elit dolor. Curabitur tincidunt, libero sed blandit suscipit, quam sem sollicitudin ex, consectetur aliquet massa libero vel lorem.</div>'
-        
         );
 });
 
 $("#myWork").click(function() {
     $("#paulPhoto").empty().append(
-        '<div class="rounded" id="picBackground"> <h1>My Work</h1> </div>'
+        '<div class="rounded" id="picBackground"> <h1>Portfolio</h1> </div>'
         );
-
 });
 
 $("#homePage").click(function() {
-    $("#paulPhoto").empty().append("<div></div>");
+    $("#paulPhoto").empty().append(
+        "<div></div>"
+        );
 });
